9c3e3926b9bb3f97ba4d8605aabdfe8f231eb67b
okaryo <okaryotr@gmail.com>
2023-03-14 01:53:09 +0900

    :sparkles: restructure og image schema and options

--- src/gatsby-node.ts
+++ src/gatsby-node.ts
@@ -4,7 +4,7 @@ import satori from 'satori'
 import sharp from 'sharp'
 import typescript from 'typescript'
 import { createFileNodeFromBuffer } from 'gatsby-source-filesystem'
-import { GatsbyNode } from 'gatsby'
+import { Node, GatsbyNode } from 'gatsby'
 import { fileURLToPath } from 'url'
 import { NodeVM, VMScript } from 'vm2'
 
@@ -14,15 +14,22 @@ const __dirname = path.dirname(__filename)
 type Options = {
   path: string
   font: string
-  emojis: {[key: string]: string}
+  width: number
+  height: number
+  graphemeImages: {[key: string]: string}
+  target_nodes: string[]
 }
 
-const defaultOptions = {
+const defaultOptions: Options = {
+  path: ``,
   font: `${__dirname}/assets/NotoSansJP-Regular.otf`,
-  emojis: {},
+  width: 1200,
+  height: 630,
+  graphemeImages: {},
+  target_nodes: ['Site', 'MarkdownRemark']
 }
 
-const generateOGPImage = async (options: Options, frontmatter: {[key: string]: unknown}): Promise<Buffer> => {
+const generateOGPImage = async (node: Node, options: Options): Promise<Buffer> => {
   const font = fs.readFileSync(options.font)
   const jsxCode = fs.readFileSync(options.path, 'utf8')
 
@@ -37,12 +44,13 @@ const generateOGPImage = async (options: Options, frontmatter: {[key: string]: u
     }
   })
   const jsCode = new VMScript(transpileModule.outputText)
-  const ogImage = vm.run(jsCode).default(frontmatter)
+  const ogImageElement = vm.run(jsCode).default(node)
 
   const svg = await satori(
-    ogImage.image,
+    ogImageElement,
     {
-      ...ogImage.options,
+      width: options.width,
+      height: options.height,
       fonts: [
         {
           name: `OG image font`,
@@ -50,47 +58,60 @@ const generateOGPImage = async (options: Options, frontmatter: {[key: string]: u
         },
       ],
       // TODO: If the original satori solves emoji problems, we will check the operation and possibly modify codes.
-      graphemeImages: options.emojis,
+      graphemeImages: options.graphemeImages,
     }
   )
   return sharp(Buffer.from(svg)).png().toBuffer()
 }
 
-export const createSchemaCustomization = ({ actions }) => {
-  actions.createTypes(`
-    type MarkdownRemark implements Node {
-      ogImage: File @link(from: "fields.ogImage")
-    }
-  `)
+export const createSchemaCustomization = ({ actions }, userOptions) => {
+  const options: Options = {
+    ...defaultOptions,
+    ...userOptions,
+  }
+
+  for (const targetNode of options.target_nodes) {
+    actions.createTypes(`
+      type ${targetNode}OgImage implements Node {
+        attributes: File
+      }
+    `)
+  }
 }
 
 export const onPostBootstrap: GatsbyNode['onPostBootstrap'] = async (
-  { actions, cache, reporter, getNodesByType, createNodeId }, pluginOptions
+  { actions, cache, reporter, getNodesByType, createNodeId }, userOptions
 ) => {
-  if (pluginOptions.path === undefined) reporter.panic(`[gatsby-plugin-satorare] \`path\` config is required.`)
+  if (userOptions.path === undefined) reporter.panic(`[gatsby-plugin-satorare] \`path\` config is required.`)
 
-  const nodes = getNodesByType('MarkdownRemark')
   const options: Options = {
     ...defaultOptions,
-    ...pluginOptions,
-    path: pluginOptions.path as string,
+    ...userOptions,
   }
 
-  for (const node of nodes) {
-    const frontmatter = node.frontmatter as {[key: string]: unknown}
-    const png = await generateOGPImage(options, frontmatter)
-    const fileNode = await createFileNodeFromBuffer({
-      buffer: png,
-      cache,
-      createNodeId,
-      ...actions,
-      ext: '.png',
-      parentNodeId: node.id,
-    })
-    actions.createNodeField({
-      name: 'ogImage',
-      node,
-      value: fileNode.id,
-    })
+  for (const targetNode of options.target_nodes) {
+    const nodes = getNodesByType(targetNode)
+    for (const node of nodes) {
+      const png = await generateOGPImage(node, options)
+      const fileNode = await createFileNodeFromBuffer({
+        buffer: png,
+        cache,
+        createNodeId,
+        ...actions,
+        ext: '.png',
+        parentNodeId: node.id,
+      })
+
+      const digest = `${node.id} >>> ${targetNode}OgImage`
+      await actions.createNode({
+        id: createNodeId(digest),
+        parent: node.id,
+        internal: {
+          type: `${targetNode}OgImage`,
+          contentDigest: digest,
+        },
+        attributes: fileNode,
+      })
+    }
   }
 }
